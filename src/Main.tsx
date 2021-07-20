e85ca8ad428d2f8c1270a4065629cb7316bee173
mily20001 <mily20001@gmail.com>
2021-07-21 00:58:10 +0200

    add basic zip reading

--- src/Main.tsx
+++ src/Main.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Main: React.FC = () => {
   return (
     <div>
-      <button onClick={() => window.api.send('app:on-fs-dialog-open')}>XDXDXD</button>
+      <button onClick={async () => {
+        const x = await window.api.send('app:on-fs-dialog-open')
+        console.log('lol', x)
+      }}>XDXDXD</button>
       <h2>XDXD lol</h2>
     </div>
   );
